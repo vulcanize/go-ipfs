feab1c11544cfb8113b20a320f062275c0c19cda
Ian Norden <iannordenn@gmail.com>
2019-06-11 12:26:33 -0500

    stash

--- misc/utility/ipfs_postgres.sh
+++ misc/utility/ipfs_postgres.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ]
   then
     echo "Env variables not provided"
-    echo "Usage: ./ipfs_postgres.sh <IPFS_PGHOST> <IPFS_PGUSER> <IPFS_PGDATABASE>"
+    echo "Usage: ./ipfs_postgres.sh <IPFS_PGHOST> <IPFS_PGPORT> <IPFS_PGUSER> <IPFS_PGDATABASE>"
     exit 1
 fi
 
 export IPFS_PGHOST=$1
-export IPFS_PGUSER=$2
-export IPFS_PGDATABASE=$3
+export IPFS_PGPORT=$2
+export IPFS_PGUSER=$3
+export IPFS_PGDATABASE=$4
 printf "${IPFS_PGUSER}@${IPFS_PGHOST} password:"
 stty -echo
 read IPFS_PGPASSWORD
